af91aa9a3a84632330f7a68b24a4619cbcca808d
Jonas Jared Jacek <mail@jonas.me>
2016-08-29 12:32:01 +0200

    added vgwort.js to /included/head.html

---
+++ js/vgwort.js
@@ -0,0 +1,3 @@
+function vgwPixelCall() {
+  document.getElementById("div_vgwpixel").innerHTML="<img src='http://vg08.met.vgwort.de/na/aaa837a5cad14a3987e305c8c3b7fc59' width='1' height='1' alt=''>";
+}
